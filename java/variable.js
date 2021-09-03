cf09846f6245b8356e7f2433291f9e1d98c1c2f5
qwer20540 <wognsko94@naver.com>
2021-09-03 12:23:09 +0900

    javastudy

--- java/variable.js
+++ java/variable.js
@@ -86,7 +86,6 @@ console.log(`value: ${x}, type: ${typeof x}`);
 const symbol1 = Symbol('id');
 const symbol2 = Symbol('id');
 console.log(symbol1 === symbol2);
-
 const gsymbol1 = Symbol.for('id');
 const gsymbol2 = Symbol.for('id');
 console.log(gsymbol1 === gsymbol2);
